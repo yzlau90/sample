ca07821bf2fd6f20cbb85b2ca8ecd6230fd61b96
yzlau90 <lau_yuan_zhi@mddi.gov.sg>
2025-05-21 07:04:49 +0000

    cleanjs

--- server.js
+++ server.js
@@ -24,24 +24,6 @@ app.get("pages/home", (req, res) => {
   res.render("pages/home"); // Renders views/home.ejs
 });
 
-app.get("pages/filter", (req, res) => {
-  res.render("pages/filter"); // Renders views/home.ejs
-});
-
-
-app.get('/', async function(req, res) {
-
-    // Try-Catch for any errors
-    try {
-        // Get all blog posts
-        const blogs = await prisma.post.findMany({
-                orderBy: [
-                  {
-                    id: 'desc'
-                  }
-                ]
-        });
-
         // Render the homepage with all the blog posts
         await res.render('pages/home', { blogs: blogs });
       } catch (error) {
@@ -59,56 +41,6 @@ app.get('/about', function(req, res) {
 app.get('/filter', function(req, res) {
     res.render('pages/filter');
 });
-
-// New post page
-app.get('/new', function(req, res) {
-    res.render('pages/new');
-});
-
-// Create a new post
-app.post('/new', async function(req, res) {
-    
-    // Try-Catch for any errors
-    try {
-        // Get the title and content from submitted form
-        const { title, content } = req.body;
-
-        // Reload page if empty title or content
-        if (!title || !content) {
-            console.log("Unable to create new post, no title or content");
-            res.render('pages/new');
-        } else {
-            // Create post and store in database
-            const blog = await prisma.post.create({
-                data: { title, content },
-            });
-
-            // Redirect back to the homepage
-            res.redirect('/');
-        }
-      } catch (error) {
-        console.log(error);
-        res.render('pages/new');
-      }
-
-});
-
-// Delete a post by id
-app.post("/delete/:id", async (req, res) => {
-    const { id } = req.params;
-    
-    try {
-        await prisma.post.delete({
-            where: { id: parseInt(id) },
-        });
-      
-        // Redirect back to the homepage
-        res.redirect('/');
-    } catch (error) {
-        console.log(error);
-        res.redirect('/');
-    }
-  });
 
 // Tells the app which port to run on
 app.listen(8080);
