80c447e76078f774a35aa20e05d5d0266bb870d1
yzlau90 <lau_yuan_zhi@mddi.gov.sg>
2025-05-21 06:36:49 +0000

    server

--- server.js
+++ server.js
@@ -14,34 +14,92 @@ app.use(express.static(__dirname + '/public'));
 // Needed for parsing form data
 app.use(express.json());       
 app.use(express.urlencoded({extended: true}));
-// add this snippet after "var express = require('express')"
-var axios = require('axios');
-
 
 // Needed for Prisma to connect to database
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
 // Main landing page
+app.get('/', async function(req, res) {
+
+    // Try-Catch for any errors
+    try {
+        // Get all blog posts
+        const blogs = await prisma.post.findMany({
+                orderBy: [
+                  {
+                    id: 'desc'
+                  }
+                ]
+        });
+
+        // Render the homepage with all the blog posts
+        await res.render('pages/home', { blogs: blogs });
+      } catch (error) {
+        res.render('pages/home');
+        console.log(error);
+      } 
+});
 
 // About page
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-// add this snippet before 
-app.get('/weather', async (req, res) => {
+// Access Filter page
+app.get('/filter', function(req, res) {
+    res.render('pages/filter');
+});
+
+// New post page
+app.get('/new', function(req, res) {
+    res.render('pages/new');
+});
+
+// Create a new post
+app.post('/new', async function(req, res) {
+    
+    // Try-Catch for any errors
     try {
-      const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
-      res.render('pages/weather', { weather: response.data });
+        // Get the title and content from submitted form
+        const { title, content } = req.body;
+
+        // Reload page if empty title or content
+        if (!title || !content) {
+            console.log("Unable to create new post, no title or content");
+            res.render('pages/new');
+        } else {
+            // Create post and store in database
+            const blog = await prisma.post.create({
+                data: { title, content },
+            });
+
+            // Redirect back to the homepage
+            res.redirect('/');
+        }
+      } catch (error) {
+        console.log(error);
+        res.render('pages/new');
+      }
+
+});
+
+// Delete a post by id
+app.post("/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        await prisma.post.delete({
+            where: { id: parseInt(id) },
+        });
+      
+        // Redirect back to the homepage
+        res.redirect('/');
     } catch (error) {
-      console.error(error);
-      res.send('Error fetching weather data');
+        console.log(error);
+        res.redirect('/');
     }
   });
- 
-// Tells the app to access filter page
-app.get('/filter', function(req, res) {
-  res.render('pages/filter');
-});  
+
+// Tells the app which port to run on
 app.listen(8080);
